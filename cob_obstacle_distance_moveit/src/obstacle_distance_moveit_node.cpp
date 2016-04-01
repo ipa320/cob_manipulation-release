744aa6d2cb21e095c00ad3de3c431e425b4f1927
ipa-fxm <fxm@ipa.fhg.de>
2016-04-01 19:14:01 +0200

    Trimmed the branch to only the cob_moveit_config sub directory

--- cob_obstacle_distance_moveit/src/obstacle_distance_moveit_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <cob_obstacle_distance_moveit/obstacle_distance_moveit.h>
-
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "obstacle_distance_node");
-
-    ObstacleDistanceMoveit odm;
-    ros::spin();
-}
