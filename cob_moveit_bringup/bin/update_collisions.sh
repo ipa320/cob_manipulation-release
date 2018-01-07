82ce8bb6bc56804bd0dcd06aba2da2e893d48009
ipa-fxm <felixmessmer@gmail.com>
2018-01-07 16:27:35 +0100

    Revert "Trimmed the branch to only the cob_collision_monitor sub directory"
    This reverts commit c51ea1f002bb8153519626a5c1ffb582c11c085c.

---
+++ cob_moveit_bringup/bin/update_collisions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+robot=$1
+
+moveit_config=`rospack find cob_moveit_config`
+config_pkg=$moveit_config/robots/$robot/moveit
+
+echo "Updating collisions in "$config_pkg
+
+rosrun moveit_setup_assistant collisions_updater --config-pkg $config_pkg --default --always --keep --trials 100000 --xacro-args '--inorder'
+
+echo "Please review changes in "$config_pkg" and provide PR!"
